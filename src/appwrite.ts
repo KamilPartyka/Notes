83888c3c02c667c4f5d1c517f6c3276240d764dd
KamilPartyka <kamilkrzysztofpartyka@gmail.com>
2026-05-07 23:38:58 +0200

    feat: add Appwrite integration and configure React Query for state management

---
+++ src/appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, TablesDB, Account } from 'appwrite';
+
+const client = new Client()
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
+export const tablesDB = new TablesDB(client);
+
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+export const NOTES_TABLE_ID = import.meta.env.VITE_APPWRITE_NOTES_TABLE_ID;
